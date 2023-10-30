caf5f8d50ea140ec2d006eb2969138df5c9ebe50
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2023-10-29 21:07:44 -0400

    Update deudores.php

--- impresion/cuotas/deudores.php
+++ impresion/cuotas/deudores.php
@@ -1,88 +1,120 @@
-<?php 
+<?php
 include_once("../../login/check.php");
-if(!empty($_GET) && $_GET['lock']==md5('lock')){
-	$titulo=$idioma['AlumnosMora'];
+if (!empty($_GET) && $_GET['lock'] == md5('lock')) {
+	$titulo = $idioma['AlumnosMora'];
 	include_once("../pdf.php");
-	class PDF extends PPDF{
-		function Cabecera(){
-			global $DesdeT,$Desde,$CursoTexto,$Or,$idioma;
-			$this->CuadroCabecera(30,$idioma['Curso'].":",30,$CursoTexto['Nombre']);
-			$this->CuadroCabecera(35,$idioma['LimiteCuota'].":",40,$Or." ".$DesdeT);			
+	class PDF extends PPDF
+	{
+		function Cabecera()
+		{
+			global $DesdeT, $Desde, $CursoTexto, $Or, $idioma;
+			$this->CuadroCabecera(30, $idioma['Curso'] . ":", 30, $CursoTexto['Nombre']);
+			$this->CuadroCabecera(35, $idioma['LimiteCuota'] . ":", 40, $Or . " " . $DesdeT);
 			$this->Pagina();
 			$this->ln();
-			$this->TituloCabecera(5,"Nº");
-			$this->TituloCabecera(50,$idioma['NombreCompleto']);
-			if($Or=="<=")$des=0;else$des=1;
-			for($i=1;$i<=$Desde-$des;$i++)
-				$this->TituloCabecera(10,$i."º C");
-			$this->TituloCabecera(20,$idioma['Total']);
-			$this->TituloCabecera(15,$idioma['Deuda']);
+			$this->TituloCabecera(5, "Nº");
+			$this->TituloCabecera(50, $idioma['NombreCompleto']);
+			if ($Or == "<=") $des = 0;
+			else $des = 1;
+			for ($i = 1; $i <= $Desde - $des; $i++)
+				$this->TituloCabecera(10, $i . "º C");
+			$this->TituloCabecera(20, $idioma['Total']);
+			$this->TituloCabecera(15, $idioma['Deuda']);
 		}
 	}
-	
+
 	include_once("../../class/cuota.php");
 	include_once("../../class/alumno.php");
 	include_once("../../class/curso.php");
-	$cuota=new cuota;
-	$alumno=new alumno;
-	$curso=new curso;
+	$cuota = new cuota;
+	$alumno = new alumno;
+	$curso = new curso;
 
-	$Desde=$_GET['Cuota'];
-	$CodCurso=$_GET['CodCurso'];
-	$Or=$_GET['Orden'];
-	echo $Orden;
-	switch($Desde){
-		case 1:{$DesdeT=$idioma["Primera"];}break;
-		case 2:{$DesdeT=$idioma["Segunda"];}break;
-		case 3:{$DesdeT=$idioma["Tercera"];}break;
-		case 4:{$DesdeT=$idioma["Cuarta"];}break;
-		case 5:{$DesdeT=$idioma["Quinta"];}break;
-		case 6:{$DesdeT=$idioma["Sexta"];}break;
-		case 7:{$DesdeT=$idioma["Septima"];}break;
-		case 8:{$DesdeT=$idioma["Octava"];}break;
-		case 9:{$DesdeT=$idioma["Novena"];}break;
-		case 10:{$DesdeT=$idioma["Decima"];}break;
-	}
-	
-	$CursoTexto=array_shift($curso->mostrarCurso($CodCurso));
-	
-	$pdf=new PDF("P","mm","letter");//612,792
-	$pdf->AddPage();
-	$i=0;
-	$MontoTotal=0;
-	
-	$alumnos=$alumno->mostrarDatosAlumnos($CodCurso);
-	foreach($alumnos as $al){
-		$i++;
-		$cuotas=$cuota->mostrarCuotasWhere("","*","CodAlumno=".$al['CodAlumno']." and Numero$Or$Desde","Numero");
-
-		if(count($cuotas)<=$Desde){
-			if($i%2==0){
-				$relleno=1;
-			}else{
-				$relleno=0;
+	$Desde = $_GET['Cuota'];
+	$CodCurso = $_GET['CodCurso'];
+	$Or = $_GET['Orden'];
+	switch ($Desde) {
+		case 1: {
+				$DesdeT = $idioma["Primera"];
 			}
-				$pdf->CuadroCuerpo(5,$i,$relleno,"R");
-				$pdf->CuadroNombre(50,$al['Paterno'],$al['Materno'],$al['Nombres'],0,$relleno);
-				$cantidad=0;
-				$MontoPagarT=0;
-				$MontoPagarTNP=0;
-				foreach($cuotas as $cuo){
-					if($cuo['Cancelado']){
-						$cantidad++;
-						$MontoPagarT+=$cuo['MontoPagar'];
-						$pdf->CuadroCuerpo(10,$cuo['MontoPagar'],$relleno,"C",0);
-					}else{
-						$MontoPagarTNP+=$cuo['MontoPagar'];
-						$pdf->CuadroCuerpo(10,"",$relleno,"C",0);
-					}
+			break;
+		case 2: {
+				$DesdeT = $idioma["Segunda"];
+			}
+			break;
+		case 3: {
+				$DesdeT = $idioma["Tercera"];
+			}
+			break;
+		case 4: {
+				$DesdeT = $idioma["Cuarta"];
+			}
+			break;
+		case 5: {
+				$DesdeT = $idioma["Quinta"];
+			}
+			break;
+		case 6: {
+				$DesdeT = $idioma["Sexta"];
+			}
+			break;
+		case 7: {
+				$DesdeT = $idioma["Septima"];
+			}
+			break;
+		case 8: {
+				$DesdeT = $idioma["Octava"];
+			}
+			break;
+		case 9: {
+				$DesdeT = $idioma["Novena"];
+			}
+			break;
+		case 10: {
+				$DesdeT = $idioma["Decima"];
+			}
+			break;
+	}
+
+	$CursoTexto = $curso->mostrarCurso($CodCurso);
+	$CursoTexto = array_shift($CursoTexto);
+
+	$pdf = new PDF("P", "mm", "letter"); //612,792
+	$pdf->AddPage();
+	$i = 0;
+	$MontoTotal = 0;
+
+	$alumnos = $alumno->mostrarDatosAlumnos($CodCurso);
+	foreach ($alumnos as $al) {
+		$i++;
+		$cuotas = $cuota->mostrarCuotasWhere("", "*", "CodAlumno=" . $al['CodAlumno'] . " and Numero$Or$Desde", "Numero");
+
+		if (count($cuotas) <= $Desde) {
+			if ($i % 2 == 0) {
+				$relleno = 1;
+			} else {
+				$relleno = 0;
+			}
+			$pdf->CuadroCuerpo(5, $i, $relleno, "R");
+			$pdf->CuadroNombre(50, $al['Paterno'], $al['Materno'], $al['Nombres'], 0, $relleno);
+			$cantidad = 0;
+			$MontoPagarT = 0;
+			$MontoPagarTNP = 0;
+			foreach ($cuotas as $cuo) {
+				if ($cuo['Cancelado']) {
+					$cantidad++;
+					$MontoPagarT += $cuo['MontoPagar'];
+					$pdf->CuadroCuerpo(10, $cuo['MontoPagar'], $relleno, "C", 0);
+				} else {
+					$MontoPagarTNP += $cuo['MontoPagar'];
+					$pdf->CuadroCuerpo(10, "", $relleno, "C", 0);
 				}
-				$pdf->CuadroCuerpo(15,number_format($MontoPagarT,2,",",""),$relleno,"R",1);
-				$pdf->CuadroCuerpo(5,$cantidad,$relleno,"C",1);
-				$pdf->CuadroCuerpo(15,number_format($MontoPagarTNP,2,",",""),$relleno,"R",1);
+			}
+			$pdf->CuadroCuerpo(15, number_format($MontoPagarT, 2, ",", ""), $relleno, "R", 1);
+			$pdf->CuadroCuerpo(5, $cantidad, $relleno, "C", 1);
+			$pdf->CuadroCuerpo(15, number_format($MontoPagarTNP, 2, ",", ""), $relleno, "R", 1);
 			$pdf->ln();
 		}
 	}
-	$pdf->Output($titulo,"I");
+	$pdf->Output($titulo, "I");
 }
-?>
