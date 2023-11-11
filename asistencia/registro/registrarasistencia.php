69718fac28d15c80624ccb04e5f12063d24b923d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2023-11-11 15:39:09 -0400

    Asistencia Reporte

--- asistencia/registro/registrarasistencia.php
+++ asistencia/registro/registrarasistencia.php
@@ -21,169 +21,170 @@ if (!empty($_POST['Codigo'])) {
 	$AtrasoAgenda = $cnf['Valor'];
 	$al = $alumno->mostrarDatosCodBarra("CodBarra='$Codigo'");
 	if (count($al) <= 0) {
-		?>
+?>
 		<div class="alert alert-error grande"><?php echo $idioma['CodigoNoAsignado'] ?></div>
 		<script language="javascript">
 			$("#Codigo").val('').focus();
 			mostrar();
 		</script>
 	<?php
+		exit();
+	}
+
+	$al = array_shift($al);
+	$cur = $curso->mostrarCurso($al['CodCurso']);
+	$cur = array_shift($cur);
+	$cArea = $cursoarea->mostrarArea($cur['CodCursoArea']);
+	$cArea = array_shift($cArea);
+	$FechaActual = date("Y-m-d");
+	$HoraActual = date("H:i:s");
+	$Dia = date("N", strtotime($FechaActual));
+	$asis = $asistencia->mostrarCodAlumnoFecha($al['CodAlumno'], $FechaActual);
+	if (count($asis) > 0) {	?>
+		<div class="alert alert-error grande"><?php echo $idioma['YaMarcoAsistencia'] ?></div>
+		<?php
+	} else {
+		switch ($Dia) {
+			case 1: {
+					$HoraInicio = $cArea['HoraInicioL'];
+					$HoraAtraso = $cArea['HoraEsperaL'];
+				}
+				break;
+			case 2: {
+					$HoraInicio = $cArea['HoraInicioM'];
+					$HoraAtraso = $cArea['HoraEsperaM'];
+				}
+				break;
+			case 3: {
+					$HoraInicio = $cArea['HoraInicioMi'];
+					$HoraAtraso = $cArea['HoraEsperaMi'];
+				}
+				break;
+			case 4: {
+					$HoraInicio = $cArea['HoraInicioJ'];
+					$HoraAtraso = $cArea['HoraEsperaJ'];
+				}
+				break;
+			case 5: {
+					$HoraInicio = $cArea['HoraInicioV'];
+					$HoraAtraso = $cArea['HoraEsperaV'];
+				}
+				break;
+			case 6: {
+					$HoraInicio = $cArea['HoraInicioS'];
+					$HoraAtraso = $cArea['HoraEsperaS'];
+				}
+				break;
+			case 7: {
+					$HoraInicio = $cArea['HoraInicioD'];
+					$HoraAtraso = $cArea['HoraEsperaD'];
+				}
+				break;
+		}
+		if ($HoraInicio == '00:00:00' || $HoraAtraso == "00:00:00") {
+		?><div class="alert alert-error grande"><?php echo $idioma['HoyNoTieneClases'] ?></div>
+			<script language="javascript">
+				$("#Codigo").val('').focus();
+				mostrar();
+			</script>
+			<?php
 			exit();
 		}
 
-		$al = array_shift($al);
-		$cur = $curso->mostrarCurso($al['CodCurso']);
-		$cur = array_shift($cur);
-		$cArea = $cursoarea->mostrarArea($cur['CodCursoArea']);
-		$cArea = array_shift($cArea);
-		$FechaActual = date("Y-m-d");
-		$HoraActual = date("H:i:s");
-		$Dia = date("N", strtotime($FechaActual));
-		$asis = $asistencia->mostrarCodAlumnoFecha($al['CodAlumno'], $FechaActual);
-		if (count($asis) > 0) {
-			?><div class="alert alert-error grande"><?php echo $idioma['YaMarcoAsistencia'] ?></div><?php
-																										} else {
-																											switch ($Dia) {
-																												case 1: {
-																														$HoraInicio = $cArea['HoraInicioL'];
-																														$HoraAtraso = $cArea['HoraEsperaL'];
-																													}
-																													break;
-																												case 2: {
-																														$HoraInicio = $cArea['HoraInicioM'];
-																														$HoraAtraso = $cArea['HoraEsperaM'];
-																													}
-																													break;
-																												case 3: {
-																														$HoraInicio = $cArea['HoraInicioMi'];
-																														$HoraAtraso = $cArea['HoraEsperaMi'];
-																													}
-																													break;
-																												case 4: {
-																														$HoraInicio = $cArea['HoraInicioJ'];
-																														$HoraAtraso = $cArea['HoraEsperaJ'];
-																													}
-																													break;
-																												case 5: {
-																														$HoraInicio = $cArea['HoraInicioV'];
-																														$HoraAtraso = $cArea['HoraEsperaV'];
-																													}
-																													break;
-																												case 6: {
-																														$HoraInicio = $cArea['HoraInicioS'];
-																														$HoraAtraso = $cArea['HoraEsperaS'];
-																													}
-																													break;
-																												case 7: {
-																														$HoraInicio = $cArea['HoraInicioD'];
-																														$HoraAtraso = $cArea['HoraEsperaD'];
-																													}
-																													break;
-																											}
-																											if ($HoraInicio == '00:00:00' || $HoraAtraso == "00:00:00") {
-																												?><div class="alert alert-error grande"><?php echo $idioma['HoyNoTieneClases'] ?></div>
-				<script language="javascript">
-					$("#Codigo").val('').focus();
-					mostrar();
-				</script>
-				<?php
-							exit();
-						}
-
-						if (strtotime($HoraActual) <= strtotime($HoraAtraso)) { //Correctamente Asistencia
-							$valores = array(
-								"CodAlumno" => $al['CodAlumno'],
-								"Tipo" => "'C'",
-								"Fecha" => "'" . $FechaActual . "'",
-								"Hora" => "'" . $HoraActual . "'",
-								"Dia" => $Dia
-							);
-							if ($asistencia->insertarRegistro($valores)) {
-								?>
-					<div class="alert alert-success grande"><?php echo $idioma['AsistenciaAlumno'] ?></div>
-				<?php
-							} else {
-								?>
-					<div class="alert alert-success grande"><?php echo $idioma['ErrorAsistencia'] ?></div>
-				<?php }
-							//echo "Asistencia";
-						} else { //Atraso
-							$valores = array(
-								"CodAlumno" => $al['CodAlumno'],
-								"Tipo" => "'A'",
-								"Fecha" => "'" . $FechaActual . "'",
-								"Hora" => "'" . $HoraActual . "'",
-								"Dia" => $Dia
-							);
-							if ($asistencia->insertarRegistro($valores)) {
-								?>
-					<div class="alert alert-danger grande"><?php echo $idioma['AtrasoAlumno'] ?></div>
-					<?php if ($AtrasoAgenda) {
-										$agendaValues = array(
-											'CodCurso' => $al['CodCurso'],
-											'CodAlumno' => $al['CodAlumno'],
-											'CodMateria' => 21, //Secretaria
-											'CodObservacion' => 8, //Atraso
-											'Fecha' => "'$FechaActual'",
-											'FechaRegistro' => "'$FechaActual'",
-											'HoraRegistro' => "'$HoraActual'",
-											'Activo' => 1,
-											'Detalle' => "''",
-											'CodUsuario' => $_SESSION['CodUsuarioLog'],
-											'Nivel' => $_SESSION['Nivel'],
-											'Resaltar' => 0,
-										);
-										$agenda->insertarRegistro($agendaValues);
-										$CodAgenda = $agenda->ultimo();
-										$mensaje = quitarTilde($idioma['Agenda']) . " $Sigla: " . quitarTilde(capitalizar($al['Paterno'])) . " " . quitarTilde(capitalizar(acortarPalabra($al['Nombres']))) . ", Obs: " . quitarTilde($obs['Nombre'], false) . " " . quitarTilde($ag['Detalle'], false) . ", " . quitarTilde($mat['Nombre'], false) . ", " . date("Y-m-d", strtotime($ag['Fecha']));
-										$mensaje = ($mensaje);
-									}
-								} else {
-									?>
-					<div class="alert alert-success grande"><?php echo $idioma['ErrorAsistencia'] ?></div>
-		<?php }
-					//echo "atraso";
+		if (strtotime($HoraActual) <= strtotime($HoraAtraso)) { //Correctamente Asistencia
+			$valores = array(
+				"CodAlumno" => $al['CodAlumno'],
+				"Tipo" => "'C'",
+				"Fecha" => "'" . $FechaActual . "'",
+				"Hora" => "'" . $HoraActual . "'",
+				"Dia" => $Dia
+			);
+			if ($asistencia->insertarRegistro($valores)) {
+			?>
+				<div class="alert alert-success grande"><?php echo $idioma['AsistenciaAlumno'] ?></div>
+			<?php
+			} else {
+			?>
+				<div class="alert alert-success grande"><?php echo $idioma['ErrorAsistencia'] ?></div>
+			<?php }
+			//echo "Asistencia";
+		} else { //Atraso
+			$valores = array(
+				"CodAlumno" => $al['CodAlumno'],
+				"Tipo" => "'A'",
+				"Fecha" => "'" . $FechaActual . "'",
+				"Hora" => "'" . $HoraActual . "'",
+				"Dia" => $Dia
+			);
+			if ($asistencia->insertarRegistro($valores)) {
+			?>
+				<div class="alert alert-danger grande"><?php echo $idioma['AtrasoAlumno'] ?></div>
+				<?php if ($AtrasoAgenda) {
+					$agendaValues = array(
+						'CodCurso' => $al['CodCurso'],
+						'CodAlumno' => $al['CodAlumno'],
+						'CodMateria' => 21, //Secretaria
+						'CodObservacion' => 8, //Atraso
+						'Fecha' => "'$FechaActual'",
+						'FechaRegistro' => "'$FechaActual'",
+						'HoraRegistro' => "'$HoraActual'",
+						'Activo' => 1,
+						'Detalle' => "''",
+						'CodUsuario' => $_SESSION['CodUsuarioLog'],
+						'Nivel' => $_SESSION['Nivel'],
+						'Resaltar' => 0,
+					);
+					$agenda->insertarRegistro($agendaValues);
+					$CodAgenda = $agenda->ultimo();
+					// $mensaje = quitarTilde($idioma['Agenda']) . " $Sigla: " . quitarTilde(capitalizar($al['Paterno'])) . " " . quitarTilde(capitalizar(acortarPalabra($al['Nombres']))) . ", Obs: " . quitarTilde($obs['Nombre'], false) . " " . quitarTilde($ag['Detalle'], false) . ", " . quitarTilde($mat['Nombre'], false) . ", " . date("Y-m-d", strtotime($ag['Fecha']));
+					// $mensaje = ($mensaje);
 				}
-				/*echo "<br>";
+			} else {
+				?>
+				<div class="alert alert-success grande"><?php echo $idioma['ErrorAsistencia'] ?></div>
+	<?php }
+			//echo "atraso";
+		}
+		/*echo "<br>";
 echo strtotime($HoraActual)." - ".$HoraActual;
 echo "<br>";
 echo strtotime($HoraInicio)." - ".$HoraInicio;
 echo "<br>";
 echo strtotime($HoraAtraso)." - ".$HoraAtraso;
 //print_r($cArea);*/
-			}
-			?>
-		<?php $ima = $folder . "imagenes/alumnos/" . $al['Foto'];
-			if (!file_exists($ima) || empty($al['Foto'])) {
-				$ima = $folder . "imagenes/alumnos/0.jpg";
-			} ?>
-		<table class="tabla">
-			<tr>
-				<td rowspan="4"><img src="<?php echo $ima ?>" class="img-polaroid" width="100" /></td>
-				<td colspan="3" class="text-info x2 espacio"><?php echo capitalizar($al['Paterno'] . " " . $al['Materno'] . " " . $al['Nombres']) ?></span></td>
-			</tr>
-			<tr>
-				<td class="text-info x2 espacio"><?php echo $cur['Nombre'] ?></span></td>
-			</tr>
-		</table>
-		<script language="javascript">
-			$("#Codigo").val('').focus();
-			mostrar();
-			<?php if ($AtrasoAgenda) {
-					?>
-				$.post('../../agenda/total/enviarsms.php', {
-					'Codigo': <?php echo $CodAgenda; ?>
-				}, function() {
-
-				})
-			<?php
-				} ?>
-		</script>
-
-	<?php
-
-
-
-
-
 	}
+	?>
+	<?php $ima = $folder . "imagenes/alumnos/" . $al['Foto'];
+	if (!file_exists($ima) || empty($al['Foto'])) {
+		$ima = $folder . "imagenes/alumnos/0.jpg";
+	} ?>
+	<table class="tabla">
+		<tr>
+			<td rowspan="4"><img src="<?php echo $ima ?>" class="img-polaroid" width="100" /></td>
+			<td colspan="3" class="text-info x2 espacio"><?php echo capitalizar($al['Paterno'] . " " . $al['Materno'] . " " . $al['Nombres']) ?></span></td>
+		</tr>
+		<tr>
+			<td class="text-info x2 espacio"><?php echo $cur['Nombre'] ?></span></td>
+		</tr>
+	</table>
+	<script language="javascript">
+		$("#Codigo").val('').focus();
+		mostrar();
+		<?php if ($AtrasoAgenda && isset($CodAgenda)) {
+		?>
+			$.post('../../agenda/total/enviarsms.php', {
+				'Codigo': <?php echo $CodAgenda; ?>
+			}, function() {
+
+			})
+		<?php
+		} ?>
+	</script>
+
+<?php
+
+
+
+
+
+}
