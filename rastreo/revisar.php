6ddc310d8d532d84466f28f2886bac68c6c396b7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2023-12-25 18:01:50 -0400

    Update revisar.php

--- rastreo/revisar.php
+++ rastreo/revisar.php
@@ -11,12 +11,31 @@ foreach ($_POST as $k => $v)
 	$Post .= "$k=$v|";
 foreach ($_SESSION as $k => $v)
 	$Session .= "$k=$v|";*/
-$Get = json_encode($_GET);
-$Post = json_encode($_POST);
-$Session = json_encode($_SESSION);
+$post = $_POST;
+$get = $_GET;
+$session = $_SESSION;
+if (isset($post['CodigoSeguimientoNotasDocente'])) {
+	$post['CodigoSeguimientoNotasDocente'] = base64_encode($post['CodigoSeguimientoNotasDocente']);
+}
+if (isset($post['CodigoSeguimientoSistema'])) {
+	$post['CodigoSeguimientoSistema'] = base64_encode($post['CodigoSeguimientoSistema']);
+}
+if (isset($post['CodigoAdicionalSistemaLogin'])) {
+	$post['CodigoAdicionalSistemaLogin'] = base64_encode($post['CodigoAdicionalSistemaLogin']);
+}
+$Get = json_encode($get);
+$Post = json_encode($post);
+$Session = json_encode($session);
 //$Ip=ip2long($Ip);
 include_once(RAIZ . "class/lograstreo.php");
 $lograstreo = new lograstreo;
+//escapar las comillas
+$Archivo = addslashes($Archivo);
+$Get = addslashes($Get);
+$Post = addslashes($Post);
+// echo $Post;
+// exit();
+$Session = addslashes($Session);
 $valores = array(
 	"`Archivo`" => "'$Archivo'",
 	"`Post`" => "'$Post'",
