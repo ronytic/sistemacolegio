ee5ea3816780350910e6a7cca4d6b38ceee088e5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2024-02-23 12:10:44 -0400

    Update nuevo.php

--- curso/modificarcurso/nuevo.php
+++ curso/modificarcurso/nuevo.php
@@ -9,8 +9,8 @@ if (isset($_POST)) {
     $config = new config;
     $Moneda = $config->mostrarConfig("Moneda", 1);
     $CodCurso = isset($_POST['CodCurso']) ? $_POST['CodCurso'] : '';
-    $cur = $curso->mostrarCurso($CodCurso);
-    $cur = array_shift($cur);
+    // $cur = $curso->mostrarCurso($CodCurso);
+    // $cur = array_shift($cur);
     $curarea = todoLista($cursoarea->mostrarTodoRegistro(), "CodCursoArea", "Nombre");
     for ($i = 1; $i <= 4; $i++) {
         $datos[$i] = $i;
@@ -23,63 +23,63 @@ if (isset($_POST)) {
         <table class="table table-bordered table-striped">
             <tr>
                 <td><?php echo $idioma['Nombre'] ?><br>
-                    <input type="text" value="<?php echo $cur['Nombre'] ?>" name="Nombre" class="span12" placeholder="1 Primaria"><br>
+                    <input type="text" value="" name="Nombre" class="span12" placeholder="1 Primaria"><br>
                     <small><?php echo $idioma['NotaNombreCurso'] ?></small>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['Abreviado'] ?><br>
-                    <input type="text" value="<?php echo $cur['Abreviado'] ?>" name="Abreviado" class="span12" placeholder="1 Prim">
+                    <input type="text" value="" name="Abreviado" class="span12" placeholder="1 Prim">
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['AreaCurso'] ?><br>
-                    <?php campo("CodCursoArea", "select", $curarea, "span12", 1, "", 0, "", $cur['CodCursoArea']) ?>
+                    <?php campo("CodCursoArea", "select", $curarea, "span12", 1, "", 0, "") ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['Paralelo'] ?><br>
-                    <?php campo("Paralelo", "select", $paralelos, "span12", 1, "", 0, "", $cur['Paralelo']) ?><br>
+                    <?php campo("Paralelo", "select", $paralelos, "span12", 1, "", 0, "") ?><br>
                     <small><?php echo $idioma['NotaParalelo'] ?></small>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['Dps'] ?><br>
-                    <?php campo("Dps", "select", array("1" => $idioma["Si"], "0" => $idioma["No"]), "span12", 1, "", 0, "", $cur['Dps']) ?>
+                    <?php campo("Dps", "select", array("1" => $idioma["Si"], "0" => $idioma["No"]), "span12", 1, "", 0, "") ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['Bimestre'] ?><br>
-                    <?php campo("Bimestre", "select", array("1" => $idioma["Si"], "0" => $idioma["No"]), "span12", 1, "", 0, "", $cur['Bimestre']) ?>
+                    <?php campo("Bimestre", "select", array("1" => $idioma["Si"], "0" => $idioma["No"]), "span12", 1, "", 0, "") ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['NotaTope'] ?><br>
-                    <input type="text" value="<?php echo $cur['NotaTope'] ?>" name="NotaTope" class="span12" placeholder="60">
+                    <input type="text" value="" name="NotaTope" class="span12" placeholder="60">
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['NotaAprobacion'] ?><br>
-                    <input type="text" value="<?php echo $cur['NotaAprobacion'] ?>" name="NotaAprobacion" class="span12" placeholder="36">
+                    <input type="text" value="" name="NotaAprobacion" class="span12" placeholder="36">
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['MontoCuotaPagar'] ?><br>
                     <div class="input-append">
-                        <input type="text" value="<?php echo $cur['MontoCuota'] ?>" name="MontoCuota" class="span12">
+                        <input type="text" value="" name="MontoCuota" class="span12">
                         <div class="add-on"><?php echo $Moneda ?></div>
                     </div>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['CantidadEtapas'] ?><br>
-                    <?php campo("CantidadEtapas", "select", $datos, "span12", 1, "", 0, "", $cur['CantidadEtapas']) ?><br>
+                    <?php campo("CantidadEtapas", "select", $datos, "span12", 1, "", 0, "") ?><br>
                     <small><?php echo $idioma['DescripcionEtapa'] ?></small>
                 </td>
             </tr>
             <tr>
                 <td><?php echo $idioma['Orden'] ?><br>
-                    <input type="text" value="<?php echo $cur['Orden'] ?>" name="Orden" class="span12" placeholder="14"><br>
+                    <input type="text" value="" name="Orden" class="span12" placeholder="14"><br>
                     <small><?php echo $idioma['DescripcionOrden'] ?></small>
                 </td>
             </tr>
