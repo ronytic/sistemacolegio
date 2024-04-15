a5b61b7a5e6eed03c9bd60c4cff78b820415aad2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2024-04-15 14:15:52 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,2 +1,5 @@
 -- Date: 2024-04-15 13:54
+UPDATE alumno SET FechaRetiro = '2024-01-01';
+ALTER TABLE alumno MODIFY COLUMN FechaRetiro date NULL;
+
 ALTER TABLE alumno ADD AccesoSistema TINYINT UNSIGNED DEFAULT 1 NULL;
