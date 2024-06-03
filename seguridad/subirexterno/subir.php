54c2523307866f381a15197bbf7ae71b929c154e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2024-06-03 15:43:40 -0400

    Corregido Seguridad

--- seguridad/subirexterno/subir.php
+++ seguridad/subirexterno/subir.php
@@ -2,58 +2,51 @@
 include_once("../../login/check.php");
 include_once("../../basededatos.php");
 include_once("../../class/config.php");
-set_time_limit(5*60);
+set_time_limit(5 * 60);
 //ini_set('max_execution_time',3000);
-$config=new config;
-$cnf=($config->mostrarConfig("IpInternet"));
-$IpInternet=$cnf['Valor'];
-$cnf=($config->mostrarConfig("PuertoInternet"));
-$PuertoInternet=$cnf['Valor'];
-$cnf=($config->mostrarConfig("UsuarioInternet"));
-$UsuarioInternet=$cnf['Valor'];
-$cnf=($config->mostrarConfig("ContrasenaInternet"));
-$ContrasenaInternet=$cnf['Valor'];
-$cnf=($config->mostrarConfig("BaseDatosInternet"));
-$BaseDatosInternet=$cnf['Valor'];
+$config = new config;
+$IpInternet = $config->mostrarConfig("IpInternet", 1);
+$PuertoInternet = $config->mostrarConfig("PuertoInternet", 1);
+$UsuarioInternet = $config->mostrarConfig("UsuarioInternet", 1);
+$ContrasenaInternet = $config->mostrarConfig("ContrasenaInternet", 1);
+$BaseDatosInternet = $config->mostrarConfig("BaseDatosInternet", 1);
 
-$local=mysql_connect($host,$user,$pass);
-mysql_select_db($database,$local);
-mysql_query("SET NAMES utf8",$local);
-$inter=mysql_connect($IpInternet.":".$PuertoInternet,$UsuarioInternet,$ContrasenaInternet);
-mysql_select_db($BaseDatosInternet,$inter);
-mysql_query("SET NAMES utf8",$inter);
-foreach($tables_export as $tabla){
+$local = mysqli_connect($host, $user, $pass, $database);
+mysqli_query($local, "SET NAMES utf8");
+$inter = mysqli_connect($IpInternet . ":" . $PuertoInternet, $UsuarioInternet, $ContrasenaInternet, $BaseDatosInternet);
+mysqli_query($inter, "SET NAMES utf8");
+foreach ($tables_export as $tabla) {
 	$consulta = "SELECT * FROM $tabla;";
-	mysql_query("TRUNCATE TABLE $tabla",$inter);
-    $respuesta = mysql_query($consulta,$local)
-    or die("No se pudo ejecutar la consulta: ".mysql_error());
-	$i=0;
-    while ($fila = mysql_fetch_array($respuesta, MYSQL_ASSOC)) {$i++;
-            $columnas = array_keys($fila);
-            foreach ($columnas as $columna) {
-                if ( gettype($fila[$columna]) == "NULL" ) {
-                    $values[] = "NULL";
-                } else {
-                    $values[] = "'".mysql_real_escape_string($fila[$columna])."'";
-                }
-            }
-			
-			if($i==1){
-            	$insert_into_query .= "INSERT INTO `$tabla` VALUES (".implode(", ", $values).")";
+	mysqli_query($inter, "TRUNCATE TABLE $tabla");
+	$respuesta = mysqli_query($local, $consulta)
+		or die("No se pudo ejecutar la consulta: " . mysqli_error($local));
+	$i = 0;
+	while ($fila = mysqli_fetch_array($respuesta)) {
+		$i++;
+		$columnas = array_keys($fila);
+		foreach ($columnas as $columna) {
+			if (gettype($fila[$columna]) == "NULL") {
+				$values[] = "NULL";
+			} else {
+				$values[] = "'" . mysqli_real_escape_string($local, $fila[$columna]) . "'";
+			}
+		}
 
-			}else{
-				$insert_into_query .= ",(".implode(", ", $values).")";
-			}
-            unset($values);
-			if($i==400){$i=0;
-				$insert_into_query.=";";
-				mysql_query($insert_into_query,$inter);
-				//echo $insert_into_query."<br><br><br>";
-				$insert_into_query="";
-			}
-    }
-	$insert_into_query.=";";
-	mysql_query($insert_into_query,$inter);
-	$insert_into_query="";
+		if ($i == 1) {
+			$insert_into_query .= "INSERT INTO `$tabla` VALUES (" . implode(", ", $values) . ")";
+		} else {
+			$insert_into_query .= ",(" . implode(", ", $values) . ")";
+		}
+		unset($values);
+		if ($i == 400) {
+			$i = 0;
+			$insert_into_query .= ";";
+			mysqli_query($inter, $insert_into_query);
+			//echo $insert_into_query."<br><br><br>";
+			$insert_into_query = "";
+		}
+	}
+	$insert_into_query .= ";";
+	mysqli_query($inter, $insert_into_query);
+	$insert_into_query = "";
 }
-?>
